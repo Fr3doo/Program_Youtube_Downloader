e2e491bc149aa280ffde8928a1e3d722c6d91e48
Fr3doo <134433826+Fr3doo@users.noreply.github.com>
2025-01-23 23:16:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# main.py
+# pyinstaller --onefile --add-data "mypy.ini;." --hidden-import "youtube_downloader" main.py
+import sys
+
+if '__annotations__' not in globals():
+    __annotations__ = {}
+print(f"Python version: {sys.version}")
+print(f"__annotations__ exists: {'__annotations__' in globals()}")
+
 import youtube_downloader
 
 # ------------------------------------------------------------------------------------------- #
@@ -21,8 +30,8 @@ while True:
             break
         
         case 1 | 5 : # 1: une vidéo avec l'audio // 5: une piste audio d'une vidéo (sound only)
-            url_video_send_user_list = []
-            url_video_send_user = youtube_downloader.demander_url_vidéo_youtube()
+            url_video_send_user_list: list[str] = []
+            url_video_send_user: str = youtube_downloader.demander_url_vidéo_youtube()
             url_video_send_user_list.append(url_video_send_user)
             if reponse_utilisateur_pour_choix_dans_menu == 1:
                 download_sound_only = False 
@@ -30,14 +39,14 @@ while True:
             youtube_downloader.download_multiple_videos(url_video_send_user_list, download_sound_only)
         
         case 2 | 6 : # 2: des vidéos avec l'audio // 6: les pistes audios de plusieurs vidéos (sound only)
-            youtube_video_links = youtube_downloader.demander_youtube_link_file()
+            youtube_video_links: list[str] = youtube_downloader.demander_youtube_link_file()
             if reponse_utilisateur_pour_choix_dans_menu == 2:
                 download_sound_only = False 
 
             youtube_downloader.download_multiple_videos(youtube_video_links, download_sound_only)
 
         case 3 | 7 : # 3: playlist (avec vidéo et audio) // 7: une playlist (sound only) 
-            url_playlist_send_user = youtube_downloader.demander_url_vidéo_youtube()
+            url_playlist_send_user: str = youtube_downloader.demander_url_vidéo_youtube()
             try:
                 link_url_playlist_youtube = youtube_downloader.Playlist(url_playlist_send_user)
             except:
@@ -46,10 +55,10 @@ while True:
                 if reponse_utilisateur_pour_choix_dans_menu == 3:
                     download_sound_only = False 
 
-                youtube_downloader.download_multiple_videos(link_url_playlist_youtube, download_sound_only)
+                youtube_downloader.download_multiple_videos(link_url_playlist_youtube, download_sound_only)  # type: ignore
              
         case 4 | 8 : # 4: channel (avec vidéo et audio) // 8: channel (sound only) 
-            url_channel_send_user = youtube_downloader.demander_url_vidéo_youtube()
+            url_channel_send_user: str = youtube_downloader.demander_url_vidéo_youtube()
             try:                    
                 link_url_channel_youtube = youtube_downloader.Channel(url_channel_send_user)
             except:
@@ -58,5 +67,5 @@ while True:
                 if reponse_utilisateur_pour_choix_dans_menu == 4:
                     download_sound_only = False
                 
-                youtube_downloader.download_multiple_videos(link_url_channel_youtube, download_sound_only)
+                youtube_downloader.download_multiple_videos(link_url_channel_youtube, download_sound_only) # type: ignore
     
